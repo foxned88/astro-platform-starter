75150be94f2659d890d74b0d8cd0f617f226b534
foxned88 <foxned88@gmail.com>
2025-07-12 19:22:50 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        server: {
+            allowedHosts: [
+                'localhost',
+                'devserver-preview--astro-test-site-1.netlify.app'
+            ]
+        }
     },
     integrations: [react()],
     adapter: netlify()
